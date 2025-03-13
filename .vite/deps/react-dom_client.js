bef1664f4514f2e8aec16234cd47f65f185a9ee7
naysa1 <naysa1@users.noreply.github.com>
2025-03-13 00:35:56 +0000

    deploy: e6b45fd11f2618c650f9ef3e6ce28019efd6efc6

--- .vite/deps/react-dom_client.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_client
-} from "./chunk-CLXPCDSX.js";
-import "./chunk-UMBEC6V5.js";
-import "./chunk-ROME4SDB.js";
-export default require_client();
-//# sourceMappingURL=react-dom_client.js.map
--- src/components/Frog.jsx
+++
@@ -1,8 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function App() {
-  return (
-    <Spline scene="https://prod.spline.design/qP82h26K9G73cGcq/scene.splinecode" />
-  );
-}
-
